4697f0241aefa257e071da8d4332e0454c6e47ad
Sebastian Kloppe <feedbackcalcest@gmail.com>
2018-12-04 19:24:34 +0100

    add register service worker function call

---
+++ src/config/registerServiceWorker.js
@@ -0,0 +1,5 @@
+export default function register() {
+  if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/service-worker.js");
+  }
+}
