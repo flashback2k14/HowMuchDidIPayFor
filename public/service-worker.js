3a0753cc7def58f08549e10d093e596a4d57c5a1
flashback2k14 <yeahdev@gmail.com>
2019-10-04 21:33:41 +0200

    2 - add manifest.json and service worker

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+const version = "1.1.0";
+const cacheName = `kep-app-${version}`;
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache
+        .addAll([`/`, `/index.html`, `/css/`, `/js/`])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches
+      .open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
